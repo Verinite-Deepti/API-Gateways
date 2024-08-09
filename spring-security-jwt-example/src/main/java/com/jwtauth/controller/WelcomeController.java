f27ade2771b50debe894d3eac37fc8b81c46ed23
Deeptiwani <Deeptiwani@host.docker.internal>
2024-08-09 17:25:40 +0530

    Initial Commit

---
+++ spring-security-jwt-example/src/main/java/com/jwtauth/controller/WelcomeController.java
@@ -0,0 +1,38 @@
+package com.jwtauth.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jwtauth.entity.AuthRequest;
+import com.jwtauth.util.JwtUtil;
+
+@RestController
+public class WelcomeController {
+
+    @Autowired
+    private JwtUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to JWT !!";
+    }
+
+    @PostMapping("/authenticate")
+    public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getUserName(), authRequest.getPassword())
+            );
+        } catch (Exception ex) {
+            throw new Exception("inavalid username/password");
+        }
+        return jwtUtil.generateToken(authRequest.getUserName());
+    }
+}
