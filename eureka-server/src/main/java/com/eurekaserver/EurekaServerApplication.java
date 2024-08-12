ca0d57d2850091cd4a86df854c93e6d057f7cae0
Deeptiwani <Deeptiwani@host.docker.internal>
2024-08-12 18:10:33 +0530

    Initial Commit

---
+++ eureka-server/src/main/java/com/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,35 @@
+package com.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+	
+	@Bean
+	public Sampler defaultSampler() {
+		return ALWAYS_SAMPLE;
+	}
+
+	private static final Sampler ALWAYS_SAMPLE = new Sampler() {
+		@Override
+		public boolean isSampled(long traceId) {
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "AlwaysSample";
+		}
+	};
+
+}
